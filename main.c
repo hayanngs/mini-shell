8400a2c5bf9093a06b25c49172fd01a501973863
hayanngs <hayannow@gmail.com>
2023-03-12 19:30:19 -0300

    Incluindo comando cd e exit

--- main.c
+++ main.c
@@ -10,7 +10,7 @@
 #define PARAM_LEN 40
 
 int read_command(char *command, char **parameters) {
-    printf("MS>");
+    printf("MS:~%s$ ", getcwd(NULL, 100));
     fgets(command, COMMAND_LEN, stdin);
     int i, cont_params = 0, index_param = 0;
     parameters[0] = malloc(PARAM_LEN);
@@ -34,27 +34,33 @@ int main() {
     while (TRUE) {
         int num_params = read_command(command, parameters);
 
-        pid_t pid = fork();
-        int status;
+        if (parameters[0][0] == 'c' && parameters[0][1] == 'd') {
+            chdir(parameters[1]);
+        } else if (parameters[0][0] == 'e' && parameters[0][1] == 'x' && parameters[0][2] == 'i' && parameters[0][3] == 't') {
+            printf("> Finalizando o mini shell.");
+            exit(0);
+        } else {
+            pid_t pid = fork();
+            int status;
 
-        switch(pid) {
-            case -1:
-                perror("> Não foi possível criar um novo processo.");
-                exit(1);
-            case 0:
-                printf("> Executando o comando '%s'.\n", parameters[0]);
-                execlp(parameters[0], parameters[0], parameters[1], 0);
-                break;
-            default:
-                waitpid(pid, &status, 0);
-                break;
-        }
-        printf("> status: %d\n", status);
-        if (WIFSIGNALED(status)){
-            printf("> Erro na execução do comando '%s'.\n", parameters[0]);
-        }
-        else if (WEXITSTATUS(status)){
-            printf("> Comando executado normalmente.\n");
+            switch (pid) {
+                case -1:
+                    perror("> Não foi possível criar um novo processo.");
+                    exit(1);
+                case 0:
+                    printf("> Executando o comando '%s'.\n", parameters[0]);
+                    execlp(parameters[0], parameters[0], parameters[1], 0);
+                    break;
+                default:
+                    waitpid(pid, &status, 0);
+                    break;
+            }
+            printf("> status: %d\n", status);
+            if (WIFSIGNALED(status)) {
+                printf("> Erro na execução do comando '%s'.\n", parameters[0]);
+            } else if (WEXITSTATUS(status)) {
+                printf("> Comando executado.\n");
+            }
         }
 
         for (int i = 0; i <= num_params; i++) {
